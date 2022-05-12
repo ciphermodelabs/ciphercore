0e90ebbd52239d1188fd5280f9fdba01f9ad36b1
nitishuplavikarcm <89805721+nitishuplavikarcm@users.noreply.github.com>
2022-05-12 14:34:25 -0700

    Documenting compiler and evaluator binaries
    * Added comments within the binaries
    
    * Addressed review comments
    
     Resolves: #41

--- ciphercore-base/src/bin/ciphercore_compile.rs
+++ ciphercore-base/src/bin/ciphercore_compile.rs
@@ -1,3 +1,4 @@
+//! Code of a binary that outputs a serialized context, which is optimized and MPC-ready, from a given context
 #![cfg_attr(feature = "nightly-features", feature(backtrace))]
 #[macro_use]
 extern crate ciphercore_base;
@@ -25,14 +26,27 @@ enum InlineModeArg {
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about=None)]
 struct Args {
-    /// Path to file that contains the serialized Context
+    /// Path to file that contains the serialized context
     context_path: String,
     #[clap(arg_enum)]
+    /// Argument to specify the inline mode required.
+    // Valid values are `simple`, `depth-optimized-default` and `depth-optimized-extreme`.
     inline_mode: InlineModeArg,
+    /// String comprising comma separated list of input parties' IDs, valid ID values include `0`, `1`, `2` OR `public` OR `secret-shared`.
     input_parties: String,
+    /// String comprising comma separated list of output parties' IDs, which could be `0`, `1`, and `2` OR `public` OR `secret-shared`.
     output_parties: String,
 }
 
+/// Returns tokens from given stringed input consisting of (1) comma separated party IDs, OR (2) "public", OR (3) "secret-shared".
+///
+/// # Arguments
+///
+/// `s` - string that encodes the information concerning participating parties
+///
+/// # Returns
+///
+/// Vector of party identifiers
 fn get_tokens(s: String) -> Result<Vec<IOStatus>> {
     let tokens: Vec<String> = s.split(',').map(|x| x.to_owned()).collect();
     if tokens.is_empty() {
@@ -65,11 +79,31 @@ fn get_tokens(s: String) -> Result<Vec<IOStatus>> {
     Ok(result)
 }
 
+/// Returns vector of tokens comprising party identifiers.
+///
+/// Wrapper function for obtaining identifier tokens.
+///
+/// # Arguments
+///
+/// 's' - string containing party identifiers
+///
+/// # Returns
+///
+/// Vector of party of identifiers
 fn parse_input_parties(s: String) -> Result<Vec<IOStatus>> {
     let tokens = get_tokens(s)?;
     Ok(tokens)
 }
 
+/// Returns vector of party identifiers given a string.
+///
+/// # Arguments
+///
+/// `s` - string containing party identifiers
+///
+/// # Returns
+///
+/// Vector of party of identifiers
 fn parse_output_parties(s: String) -> Result<Vec<IOStatus>> {
     let tokens = get_tokens(s)?;
     if tokens.len() == 1 {
@@ -101,6 +135,15 @@ fn parse_output_parties(s: String) -> Result<Vec<IOStatus>> {
     }
 }
 
+/// Returns internal inline mode based on the user-given argument
+///
+/// # Argument
+///
+/// `mode` - user-given inline mode argument
+///
+/// # Returns
+///
+/// Inline mode
 fn get_inline_mode(mode: InlineModeArg) -> InlineMode {
     match mode {
         InlineModeArg::Simple => InlineMode::Simple,
@@ -113,18 +156,44 @@ fn get_inline_mode(mode: InlineModeArg) -> InlineMode {
     }
 }
 
+/// Returns an unseeded simple evaluator instance
+///
+/// # Returns
+///
+/// Evaluator
 fn get_evaluator() -> Result<SimpleEvaluator> {
     SimpleEvaluator::new(None)
 }
 
+/// This binary prints to stdout the serialized context, which is optimized and MPC-ready, from a given context.
+///
+/// # Arguments
+///
+/// * `context_path` - path to file that contains the serialized context
+/// * `inline_mode` - argument to specify the inline mode required, valid values are `simple`, `depth-optimized-default` and  `depth-optimized-extreme`
+/// * `input_parties` - string comprising of either a comma separated list of input parties' IDs, valid ID values include `0`, `1`, `2` OR `public` OR `secret-shared`
+/// * `output_parties` - string comprising comma separated list of output parties' IDs, which could be `0`, `1`, and `2` OR `public` OR `secret-shared`
+///
+/// # Usage
+///
+/// < this_binary > <context_path> <inline_mode> <input_parties> <output_parties>
 fn main() {
+    // Initialize a logger that collects information about errors and panics within CipherCore.
+    // This information can be accessed via RUST_LOG.
     env_logger::init();
+    // Execute CipherCore code such that all the internal errors are properly formatted and logged.
     execute_main(|| -> Result<()> {
+        // Parse the input arguments
         let args = Args::parse();
+        // Read the entire file containing a serialized context as a string
         let serialized_context = fs::read_to_string(&args.context_path)?;
+        // Deserialize into a context object from the serialized context string
         let context = serde_json::from_str::<Context>(&serialized_context)?;
+        // Parse the input party information
         let input_parties = parse_input_parties(args.input_parties)?;
+        // Parse the output party information
         let output_parties = parse_output_parties(args.output_parties)?;
+        // Obtain the compiled context
         let compiled_context = compile_context(
             context,
             input_parties,
@@ -135,6 +204,7 @@ fn main() {
             },
             get_evaluator,
         )?;
+        // Print the serialized and compiled context to stdout
         println!("{}", serde_json::to_string(&compiled_context).unwrap());
         Ok(())
     });
--- ciphercore-base/src/bin/ciphercore_evaluate.rs
+++ ciphercore-base/src/bin/ciphercore_evaluate.rs
@@ -1,3 +1,4 @@
+//! Code of a binary printing evaluated result of a given serialized context on given context inputs
 #![cfg_attr(feature = "nightly-features", feature(backtrace))]
 
 use ciphercore_base::errors::Result;
@@ -12,30 +13,58 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about=None)]
 struct Args {
+    /// Path to a file containing serialized context
     context_path: String,
+    /// Path to a file containing serialized inputs
     inputs_path: String,
     #[clap(long)]
+    /// (optional) Boolean to indicate if the output is to be revealed for secret-shared outputs
     reveal_output: bool,
 }
 
+/// This binary evaluates a given context over the provided inputs using a simple evaluator.
+///
+/// For a secret-shared output, output is revealed if the `reveal_output` boolean binary argument is set to `true`.
+///
+/// # Arguments
+///
+/// * `context_path` - path to a serialized context
+/// * `inputs_path` - path to serialized input(s)
+/// * `reveal_output` - boolean to indicate if output is to be revealed
+///
+/// # Usage
+///
+/// < this_binary > [--reveal-output] <CONTEXT_PATH> <INPUTS_PATH>
 fn main() {
+    // Initialize a logger that collects information about errors and panics within CipherCore.
+    // This information can be accessed via RUST_LOG.
     env_logger::init();
+    // Execute CipherCore code such that all the internal errors are properly formatted and logged.
     execute_main(|| -> Result<()> {
+        // Parse the input arguments
         let args = Args::parse();
+        // Read the entire file containing a serialized context as a string
         let serialized_context = fs::read_to_string(&args.context_path)?;
+        // Deserialize into a context object from the serialized context string
         let raw_context = serde_json::from_str::<Context>(&serialized_context)?;
+        // Read the entire file containing serialized inputs as a string
         let json_inputs = fs::read_to_string(&args.inputs_path)?;
+        // Parse input string to obtain JSON value object(s)
         let json_inputs = json::parse(&json_inputs)?;
+        // Parse inputs to obtain a vector of typed values, i.e., pair of type, and its value
         let inputs = parse_json_array(&json_inputs)?;
+        // Use the simple evaluator to obtain the typed result value
         let result = get_evaluator_result(
             raw_context,
             inputs,
             args.reveal_output,
             SimpleEvaluator::new(None)?,
         )?;
+        // Depending on the input argument, print whether the output is revealed
         if args.reveal_output {
             eprintln!("Revealing the output");
         }
+        // Print the serialized result to stdout
         println!("{}", result.to_json()?);
         Ok(())
     });
